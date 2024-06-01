073ca9a8a5a15a0bfb84a7f01d915f78fb6a3962
felipelima3110 <felipelima3110@gmail.com>
2024-06-01 14:32:51 -0300

    variáveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,31 @@
+/**
+ * Tutorial Node.Js
+ * VAriáveis
+ * 
+ */
+
+console.log ("________________")
+const nome = "José de Assis"
+console.log(nome)
+console.log(typeof nome)
+
+const pi = 3.14
+console.log(pi)
+console.log(typeof pi)
+
+var time = "Corinthians"
+console.log(time)
+console.log ("________________")
+console.log(typeof time)
+var time = "São Paulo"
+console.log (time)
+/**
+ * O var permite a redeclaração de variáveis
+ * O let é melhor pois só permite a alteração
+ */
+
+let linguagem = "Java"
+console.log(linguagem)
+linguagem = "JavaScript"
+console.log ("________________")
+console.log (linguagem)
